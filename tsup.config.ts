3da631e84d85d1629ae6335222f659c52b9ab641
FurryR <awathefox@gmail.com>
2024-09-28 08:32:58 +0800

    :bug: fix(extension): incorrect extension metadata

--- tsup.config.ts
+++ tsup.config.ts
@@ -1,19 +1,18 @@
 import { defineConfig } from 'tsup'
 
 export default defineConfig({
-  name: 'scratch-ext', // Replace it with your extension name
+  name: 'kylin',
   entry: ['src/index.ts', 'src/index.js'],
   target: ['esnext'],
   format: ['iife'],
   outDir: 'dist',
   banner: {
     // Replace it with your extension's metadata
-    js: `// Name: FurryR's example Extension
-// ID: newExtension
-// Description: Scaffolding for Advanced Scratch extensions.
-// By: You
-// Original: Me
-// License: MPL-2.0
+    js: `// Name: 
+// ID: kylin
+// Description: Tool to obfuscate your Scratch project.
+// By: FurryR
+// License: AGPL-3.0-only
 `
   },
   platform: 'browser',
